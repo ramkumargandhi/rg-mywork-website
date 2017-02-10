0a8abe7ec22a61c97764f4e6fbac4b4360a9d7a3
Ramkumar Gandhi <ramkumar.gandhi@outlook.com>
2017-02-10 00:27:53 -0500

    Inital blank MVC site commit

---
+++ rg-mywork-website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rg_mywork_website.Startup))]
+namespace rg_mywork_website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
